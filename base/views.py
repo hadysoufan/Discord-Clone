99f50a0fc669252d3d308b75e1e040ef2fa6f826
hadysoufan <hadisoufan74@gmail.com>
2023-03-18 14:11:18 +0200

    Using object manager to get data from panel

--- base/views.py
+++ base/views.py
@@ -1,25 +1,15 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn Python'},
-    {'id': 2, 'name': 'Welcome to Java'},
-    {'id': 3, 'name': 'Getting started with JavaScript'},
-    {'id': 4, 'name': 'Docker, lets know about it'},
-]
-
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
