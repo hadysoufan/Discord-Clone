8051f92c67b89f82bf84f9bef838c650eaa2ee69
hadysoufan <hadisoufan74@gmail.com>
2023-03-18 13:00:06 +0200

    Creating simple object and context dictionary

--- base/views.py
+++ base/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Welcome to Java'},
+    {'id': 3, 'name': 'Getting started with JavaScript'},
+    {'id': 4, 'name': 'Docker, lets know about it'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
