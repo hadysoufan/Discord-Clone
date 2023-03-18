2e1ff8331874f5babbfc2dd182fb72350cff2a31
hadysoufan <hadisoufan74@gmail.com>
2023-03-18 21:16:41 +0200

    Creating url and view for user profile

--- base/views.py
+++ base/views.py
@@ -99,9 +99,14 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
-def userProfile(request):
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
 
-    context = {}
+    context = {'user': user, 'rooms': rooms,
+               'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 
